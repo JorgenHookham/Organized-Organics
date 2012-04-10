446b525a72b6d51967e43b1ee5a24c5120f64a2b
Jørgen Hookham <JorgenHookham@Gmail.com>
2012-04-09 18:27:16 -0700

    update db config.

--- includes/db.php
+++ includes/db.php
@@ -3,6 +3,6 @@
 	$dbconfig['host'] = 'localhost';
 	$dbconfig['database'] = 'organizedorganics';
 	$dbconfig['user'] = 'oo';
-	$dbconfig['password'] = 'interactivedesignlab';
+	$dbconfig['password'] = '5e5bSHu9S5wjRVbfkqwj';
 
 ?>
